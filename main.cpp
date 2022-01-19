1b5a2f96ca698c241e72530b614537dbd341fc49
Jin <jinchuang1999@gmail.com>
2022-01-19 15:29:18 +0800

    new feature: Only the 2 lines will be reserved.

--- main.cpp
+++ main.cpp
@@ -19,16 +19,44 @@ char displayWindowName[] = "Detected Lines (in red) - Probabilistic Line Transfo
 int ts = 0;
 const int max_ts = 100;
 
+vector<Vec4i> lineFilter(vector<Vec4i> linesP)
+{
+    vector<Vec4i> linesFiltered;
+    double slope_min = 999, slope_max = -999;
+    int slope_min_idx = 0, slope_max_idx = 0;
+
+    for (int i = 0; i < linesP.size(); i++)
+    {
+        double delta_x = linesP[i][0] - linesP[i][2];
+        double delta_y = linesP[i][1] - linesP[i][3];
+        double slope = delta_x / delta_y;
+        if (slope_min > slope)
+        {
+            slope_min = slope;
+            slope_min_idx = i;
+        }
+        if (slope_max < slope)
+        {
+            slope_max = slope;
+            slope_max_idx = i;
+        }
+    }
+    linesFiltered.push_back(linesP[slope_min_idx]);
+    linesFiltered.push_back(linesP[slope_max_idx]);
+    return linesFiltered;
+}
+
 static void HoughLine(int, void*)
 {
     // Probabilistic Line Transform
     vector<Vec4i> linesP; // will hold the results of the detection
     HoughLinesP(dst, linesP, 1, CV_PI / 180, ts, 50, 10); // runs the actual detection
-    
+    cout << "Lines detected: " << linesP.size() << endl;
+    vector<Vec4i> lf = lineFilter(linesP);
     // Draw the lines
-    for(size_t i = 0; i < linesP.size(); i++)
+    for(size_t i = 0; i < lf.size(); i++)
     {
-        Vec4i l = linesP[i];
+        Vec4i l = lf[i];
         line(cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, LINE_AA);
     }
     imshow(displayWindowName, cdstP);
